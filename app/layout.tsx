696b5fe728d0b817eed6e60d839feb8384361394
Dina <dinagiacobbi@gmail.com>
2024-07-01 10:21:03 -0800

    Adding provider themes and styles for dark light modes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/navbar/Navbar'
+import Providers from './providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,13 +17,15 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
+        <Providers>
         <Navbar />
         
         <main className='container py-10'>
           {children}
         </main>
+        </Providers>
       </body>
     </html>
   )
