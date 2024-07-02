24aa6cefc966898ef5272c635088d9a242d8bd14
Dina <dinagiacobbi@gmail.com>
2024-07-02 13:09:10 -0800

    Adding start of Auth with clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/navbar/Navbar'
 import Providers from './providers'
+import {ClerkProvider } from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,6 +18,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
+   <ClerkProvider> 
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <Providers>
@@ -28,5 +30,6 @@ export default function RootLayout({
         </Providers>
       </body>
     </html>
+    </ClerkProvider>
   )
 }
